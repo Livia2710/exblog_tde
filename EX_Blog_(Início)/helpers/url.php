eed289eddf996f08566ce3915c938b52bd187930
Livia2710 <livia.sesi166@gmail.com>
2023-10-05 14:36:30 -0300

    wer

--- EX_Blog_(Início)/helpers/url.php
+++ EX_Blog_(Início)/helpers/url.php
@@ -1,5 +1,5 @@
 <?php
 //Constrói a URL base do site combinando o protocolo,
 //nome do servidor. diretorio atual e barra final
-$BASE_URL ="http://" .$_SERVER['SERVER NAME'].dirname($_SERVER["REQUEST_URI"]. '?'). '/';
+$BASE_URL ="http://" .$_SERVER['SERVER NAME'].dirname($_SERVER["REQUEST_URI"]. '?'). '';
 ?>
